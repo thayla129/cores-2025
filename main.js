f0ac7b21d55da230d580409dfbbdb85d1be881e3
THAYLA AMORIM MATEUS <24122553@CFP127ALUNO.LOCAL>
2025-01-30 15:35:51 -0300

    Aplicação Concluida

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 "use strict"
 
-const botaoTrocarCor = document.getElementById('trocar cor')
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+function trocarCor(){
+
+    const cor = document.getElementById('cor').value
+
+    document.documentElement.style.setProperty('--cor-fundo', cor)
+}
+
 
 botaoTrocarCor.addEventListener('click', trocarCor)
